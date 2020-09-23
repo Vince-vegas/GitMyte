562b8b907af202a1200061e4d9cc574b690f84c6
vince vegas <sanchezvince769@gmail.com>
2020-09-23 15:15:01 +0800

    Responsive Charts

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -24,6 +24,7 @@ const UserPage = (props) => {
   } = githubContext;
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     const queryStr = new URLSearchParams(props.location.search);
     const getName = queryStr.get('name');
 
